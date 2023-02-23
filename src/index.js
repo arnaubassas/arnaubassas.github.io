1a408c299f1e0c46377d782bbf59f42df8cbf059
arnaubassas <arnau_b17@hotmail.com>
2023-02-23 17:03:46 +0100

    upgrade gh pages with hashrouter

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Portfolio />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
